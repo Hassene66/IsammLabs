18dc2e758146bb0dc820f9536725205e3a3a2839
Hassene Ayoub <53129325+Hassene66@users.noreply.github.com>
2022-03-06 00:26:25 +0100

    adding fontend  login screen

---
+++ src/Config/color.js
@@ -0,0 +1,10 @@
+export default {
+  primary: '#C84648',
+  secondary: '#4ecdc4',
+  black: '#000',
+  white: '#fff',
+  dark: '#0c0c0c',
+  medium: '#6e6969',
+  light: '#ebebeb',
+  danger: '#ff5252',
+};
