a5c74fc2c6a32995671415190c994924ae2a4fa7
Hassene Ayoub <53129325+Hassene66@users.noreply.github.com>
2022-09-16 11:46:32 +0100

    removed android from gitignore

---
+++ android/app/src/main/java/com/isamm_labs/MainActivity.java
@@ -0,0 +1,38 @@
+package com.isamm_labs;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import com.facebook.react.ReactActivityDelegate;
+//import com.zoontek.rnbootsplash.RNBootSplash;
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.id.lottie); // here
+    SplashScreen.setAnimationFinished(true); // If you want the animation dialog to be forced to close when hide is called, use this code
+    super.onCreate(savedInstanceState);
+  }
+  @Override
+  protected String getMainComponentName() {
+    return "Isamm_labs";
+  }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        // RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        super.loadApp(appKey);
+      }
+    };
+  }
+}
